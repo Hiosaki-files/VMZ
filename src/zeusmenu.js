06137c018d603790faeae2706c5efd541e1712b8
Hiosaki-files <79849089+Hiosaki-files@users.noreply.github.com>
2021-03-23 08:13:04 -0300

    Add files via upload

---
+++ src/zeusmenu.js
@@ -0,0 +1,41 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do skyhiro:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  Modificado por *Paulo.H*
+  WA.me/5538988313755
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
+
+
